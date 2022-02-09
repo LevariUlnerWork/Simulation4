202894bb11d16d916280b998952228c3cc2fa116
levAriUlnerWork <levariulner@gmail.com>
2022-02-09 18:46:57 +0200

    Daily

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
-import pickle
-import functools
-import matplotlib.pyplot as plt
+import yfinance
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import functools
 import seaborn as sns
-import yfinance
 from random import randint
 import scipy.stats
 
-START_DATE = "1980-01-01"
-END_DATE =
+STOCKS_LIST = [('BMY', (1/7)), ('CVS', (1/7)), ('DAL', (1/7)), ('FB', (1/7)), ('UG', (1/7)), ('SWKS', (1/7)), ('MUV', (1/7))]
 
+START_DATE = "2013-01-01"
+END_DATE = "2021-12-31"
 
+#------------------------------- Question 1 ---------------------------------------------
 
+def daily_stock_pct():
+    returns = pd.DataFrame({})
+    for t in STOCKS_LIST:
+        name = t[0]
+        ticker = yfinance.Ticker(name)
+        data = ticker.history(interval="1d",start=START_DATE, end=END_DATE)
+        data['return_%s' % (name)] = data['Close'].pct_change(1)*100
+        returns = returns.join(data[['return_%s' % (name)]],
+                               how="outer").dropna()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(returns)
+    return returns
+
+def daily_stock():
+    returns = pd.DataFrame({})
+    for t in STOCKS_LIST:
+        name = t[0]
+        ticker = yfinance.Ticker(name)
+        data = ticker.history(interval="1d",start=START_DATE, end=END_DATE)
+        data['return_%s' % (name)] = data['Close'].pct_change(1)
+        returns = returns.join(data[['return_%s' % (name)]],
+                               how="outer").dropna()
+
+    # print(returns)
+    return returns
+
+def hist_stock():
+    df = daily_stock_pct()
+    for i in df:
+        df_new = df[i]
+        plt.hist(df_new,bins=50, color='#0080FF', edgecolor='black', linewidth=1.8, alpha=0.65)
+        plt.title(i)
+        plt.xlabel("Changes (Precentage)")
+        plt.ylabel("Amount of Days")
+        plt.show()
