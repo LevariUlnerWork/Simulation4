59310d150f5e05735cd1bc6227d2e38ff2780c6a
levAriUlnerWork <levariulner@gmail.com>
2022-02-09 17:57:16 +0200

    Start

---
+++ main.py
@@ -0,0 +1,19 @@
+import pickle
+import functools
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import yfinance
+from random import randint
+import scipy.stats
+
+START_DATE = "1980-01-01"
+END_DATE =
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
